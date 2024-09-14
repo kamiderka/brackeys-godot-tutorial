63b3b2f3947700fb901d5122cc2bbcf84d86e511
Kamil Kordecki <kamil@kordecki.dev>
2024-09-14 23:01:21 +0200

    feat: Add coin pickup sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
 @onready var game_manager: Node2D = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin")
+	animation_player.play("pickup")
 	game_manager.add_point()
-	queue_free()
+	
